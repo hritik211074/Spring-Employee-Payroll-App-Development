d4c198f080fa4a07036b6932f1a5e87890bd5e6e
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-23 18:00:48 +0530

    [Hritik] adding the uc02 folder into UC02

--- src/main/java/com/example/employeepayrollapp/uc02_demonstrate_various_http_method/PayrollApplication.java
+++ src/main/java/com/example/employeepayrollapp/uc02_demonstrate_various_http_method/PayrollApplication.java
@@ -1,4 +1,13 @@
 package com.example.employeepayrollapp.uc02_demonstrate_various_http_method;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PayrollApplication {
+
+    public static void main(String[] args) {
+        // This will start the Spring Boot application
+        SpringApplication.run(PayrollApplication.class, args);
+    }
 }
--- src/main/java/com/example/employeepayrollapp/uc02_demonstrate_various_http_method/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeepayrollapp/uc02_demonstrate_various_http_method/repository/EmployeeRepository.java
@@ -1,4 +1,6 @@
 package com.example.employeepayrollapp.uc02_demonstrate_various_http_method.repository;
+import com.example.payroll.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
