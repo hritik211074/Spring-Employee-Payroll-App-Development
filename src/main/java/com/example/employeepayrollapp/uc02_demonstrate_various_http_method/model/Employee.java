4f8de95f5b77995cec69ff135c44d7fd6a10fff0
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-23 17:58:09 +0530

    [Hritik] adding the important file

---
+++ src/main/java/com/example/employeepayrollapp/uc02_demonstrate_various_http_method/model/Employee.java
@@ -0,0 +1,4 @@
+package com.example.employeepayrollapp.uc02_demonstrate_various_http_method.model;
+
+public class Employee {
+}
---
+++ src/main/java/com/example/employeepayrollapp/uc02_demonstrate_various_http_method/repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.example.employeepayrollapp.uc02_demonstrate_various_http_method.repository;
+
+public class EmployeeRepository {
+}
