24bbee64cd7bdb06cf6ee0c857345a2b892a270a
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-24 23:27:55 +0530

    [Hritik] changes in this file

--- src/main/java/com/example/employeepayrollapp/uc01_employee_paayroll_ui/model/Employee.java
+++ src/main/java/com/example/employeepayrollapp/uc01_employee_paayroll_ui/model/Employee.java
@@ -1,7 +1,7 @@
 package com.example.employeepayrollapp.uc01_employee_paayroll_ui.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
